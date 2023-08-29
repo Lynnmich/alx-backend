10621d3aac015c5e6bb838016b74ae278dd114ae
Lynnmich <lynnenmichuki@gmail.com>
2023-08-29 13:46:53 +0300

    Task 0

---
+++ 0x02-i18n/templates/0-app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Basic flask app"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """App route for index.html"""
+    return render_template("0-index.html")
