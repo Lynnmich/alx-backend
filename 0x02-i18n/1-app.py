96c5f321c5900469b3cabc2fbf31b7ab9afb88ee
Lynnmich <lynnenmichuki@gmail.com>
2023-08-29 15:14:27 +0300

    Task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,7 +8,7 @@ app = Flask(__name__, static_url_path="")
 babel = Babel(app)
 
 
-class Config:
+class Config(object):
     """Config class for babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
